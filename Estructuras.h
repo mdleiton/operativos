d8648a3b405caf01ba5a39325d1bdb734c1a3f44
mdleiton <mau_leiton96@hotmail.com>
2019-11-22 00:57:57 -0500

    [add] funcion para imprimir matriz

--- Estructuras.h
+++ Estructuras.h
@@ -2,11 +2,23 @@
 #define  FILLED     0
 #define  TAKEN      1
 #define ID_MC 123
+#define INFINITY 9999
+#define MAX 14
 
 
 struct Datos {
 	int  status;
-	int  data[4];
-	int matriz[10][10];
+	int grafo[MAX][MAX];
+	int cantidadNodos;
 };
 
+
+void imprimirMatriz(int grafo[MAX][MAX]){
+	for(int a=0; a<MAX; a++){    
+        for(int b=0; b<MAX; b++){
+          printf("-%d-",grafo[a][b]);
+        }
+     printf("\n");
+     }
+
+}
