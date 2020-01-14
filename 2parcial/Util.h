ab85fa8b7e43e4052b6351cc2a7a9e24a8e412aa
mdleiton <mau_leiton96@hotmail.com>
2020-01-13 23:31:02 -0500

    [add] estados de un brazo robotico

---
+++ 2parcial/Util.h
@@ -0,0 +1,20 @@
+//
+// Created by mdleiton on 13/1/20.
+//
+/**
+ * Esquemas de funcionamiento
+ * */
+#define ESQUEMA_PRIMERO_DISPONIBLE 1
+#define ESQUEMA_IGUAL_X_PEDIDOS 2
+#define ESQUEMA_MENOR_ITEM_PENDIENTES 3
+
+#define PRIORIDAD_ESQUEMA_PRIMERO_DISPONIBLE 0
+
+
+/**
+ * Estados de un Brazo Robótico
+ * */
+#define BRAZO_DISPONIBLE 1 //INICIADO
+#define BRAZO_SUSPENDIDO 2
+#define BRAZO_TERMINADO 3
+#define BRAZO_OCUPADO 4
