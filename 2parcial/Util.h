38adce602801b7696e29cb6bc89011b67e13d559
mdleiton <mau_leiton96@hotmail.com>
2020-01-23 08:00:46 -0500

    [add] definicion de senales y funcion para envio de senales

--- 2parcial/Util.h
+++ 2parcial/Util.h
@@ -1,6 +1,10 @@
 //
 // Created by mdleiton on 13/1/20.
 //
+
+#include <sys/syscall.h>
+#include <signal.h>
+
 /**
  * Esquemas de funcionamiento
  * */
@@ -26,9 +30,44 @@
 #define PEDIDO_PARCIAL 1
 #define PEDIDO_FINALIZADO 2
 
+/* SEÑALES	*/
+#define EXITPROGRAMA_PLANIFICADOR 31
+#define EXITPROGRAMA_GENERADOR 32
+#define INITPROGRAMA_PLANIFICADOR 40
+#define INITPROGRAMA_GENERADOR 34
+
+#define ID_MC 124
+
 /**
  * Estructura de información del sisteam
  */
 struct Informacion{
     int pedidosFinalizados;
 };
+
+/**
+ * Estructura de comunicacion entre procesos
+ */
+struct Proceso{
+    long pidGenerador;
+    long pidPlanificador;
+    sem_t mutex;
+};
+
+/*
+ *  Función que envia una señal a un proceso.
+ *  @param pid identificador del programa.
+ *  @param signalValue entero que se puede enviar en la señal.
+ *  @param signalId el tipo de señal que se envia.
+ *  @return 0 en caso de que se envie y se reciba la señal, -1 cuando ocurra algún error.
+ */
+int enviarSenal(long pid, int signalValue, int signalId){
+    if (pid>0){
+        int estado;
+        union sigval mysigval;
+        mysigval.sival_int = signalValue;
+        int send = sigqueue(pid, signalId, mysigval);
+        return send;
+    }
+    return -1;
+}
